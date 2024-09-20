abf36d3ae61212b225cbe05d0ed6dd782e2c93e0
folushojimi <iamrblogger@gmail.com>
2024-09-20 23:11:20 +0100

    javascript operator

---
+++ script.js
@@ -0,0 +1,100 @@
+//CONDITIONAL STATEMENT - They are block of code written to determine 
+//should some certain operations or effect takes place in our code
+//we basically have 2 types of conditional statemnet in javascript.IF STATEMNT & SWITCH STATEMENT
+
+
+
+// IF STATEMENT 
+ 
+//  const age = 11
+
+//  if (age > 18) {
+// console.log('You may enter');
+//  }else if (age == 18){
+//     console.log('You just turned 18')
+//  }else {
+//     console.log('Grow up!')
+//  }
+
+
+//  let light = 0
+
+//  if (light === 14){
+//    console.log('blue')
+//    }else if (light > 14){
+//    console.log('red')
+//    }else{
+//       console.log('switch off the light')
+//    }
+
+let food = 10
+
+
+if (food > 12) {
+   console.log(`bring ${food} food for people`)
+}else {
+   console.log('NO, FOOD HERE!')
+}
+
+
+// let workTime = prompt('what is the recommended work time', '');
+ 
+// if (workTime === '8am') {
+// alert('punctual')
+// }else if(workTime === '8:30am'){
+//    alert('not bad you are still welcome')
+// }else{
+//    alert('null')
+// }
+
+// TEST FOR SEMICOLON
+// console.log('this will show an error')
+// [1,2].forEach(alert)
+
+// alert('this code will work now');
+// [1,2].forEach(alert)
+
+// practice on if statement 
+
+// 1. A prompted program for the best laptop memory for a graphic designer
+//prompt converted to number
+// let devGroup = Number(prompt('Whats the best memory for a graphic designer',))
+
+//     if (devGroup === 100){
+//     alert('Super Good Laptop')
+//     }else if(devGroup <= 99){
+//     alert('Manageable Laptop')
+//     }else if(devGroup > 100){
+//         alert('beastmode')
+//     }else{
+//     alert('null')
+//     }
+
+//2. A basic console program for verifying only student 18 and above for a boxing class
+
+// let boxingAge = 10
+
+// if (boxingAge >= 18){
+// console.log('Good fit')
+// }else{
+//    console.log('Sorry we cannot take you in')
+// }
+
+const ayenfe = 'nobody'
+
+if (ayenfe === 'myPartner'){
+console.log(true)
+}else if (ayenfe === 'myfriend'){
+   console.log(true)
+}else{
+   console.log(false)
+}
+
+
+// ADVANCE IF STATEMENT
+
+
+// SWITCH STATMENT
+
+
+//TERNARY OPERATOR
